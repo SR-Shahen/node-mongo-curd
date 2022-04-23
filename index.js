eb419214be1e4cf06877691024001ddd91bcdd1e
SR-Shahen <s.r.shahenda@gmail.com>
2022-04-23 20:12:30 +0600

    delete user from database and client side  use server side

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const objectId = require('mongodb').ObjectId;
+
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -25,20 +27,45 @@ async function run() {
         await client.connect();
         const userCollection = client.db("foodExpress").collection("user");
 
-        app.post('/user', (req, res) => {
+        // Get user
+        app.get('/user', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+
+
+        // Post user: create a new user
+
+        app.post('/user', async (req, res) => {
             const newUser = req.body;
             console.log('adding newUser', newUser);
-            res.send({ success: "success" })
+            const result = await userCollection.insertOne(newUser);
+            res.send(result)
         });
+
+        // Delete user
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: objectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result)
+        })
+
     }
     finally {
         // await client.close()
     }
 }
 run().catch(console.dir);
-app.get('/user', (req, res) => {
+app.get('/', (req, res) => {
     res.send("react-with node mongodb open");
 })
+// app.get('/user', (req, res) => {
+//     res.send("react-with node mongodb open");
+// })
 
 app.listen(port, () => {
     console.log("mongo curd run hoiche", port);
